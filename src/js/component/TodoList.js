021d00640d9a43d610f5e623d2140272a81e1051
Fernando Funez <ferfunes@gmail.com>
2019-12-20 22:23:40 +0000

    updates

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -7,7 +7,9 @@ export class TodoList extends React.Component {
 		const { items, clearList, handleDelete, handleEdit } = this.props;
 		return (
 			<ul className="list-group my-5">
-				<h3 className="text-capitalize text-center">todo list</h3>
+				<h3 className="text-capitalize text-center text-info">
+					todo list
+				</h3>
 				{items.map(item => {
 					return (
 						<TodoItem
