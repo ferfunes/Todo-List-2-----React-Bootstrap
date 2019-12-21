22c1255039c07f732ab5a9ab03edbe52b6802b51
Fernando Funez <ferfunes@gmail.com>
2019-12-21 16:09:51 +0000

    updates

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -65,7 +65,7 @@ export class App extends React.Component {
 			<div className="container">
 				<div className="row">
 					<div className="col-10 mx-auto col-md-8 mt-4">
-						<h3 className="text-capitalize text-center text-info">
+						<h3 className="text-capitalize text-center ">
 							todo input
 						</h3>
 						<TodoInput
